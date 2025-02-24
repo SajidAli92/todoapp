85ea6a330106e2f76d8327f9015ebd6d472f4fef
Sajid Ali <sajidali2ns@gmail.com>
2025-02-24 16:59:31 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./components/Header"
+import Todo from "./components/Todo"
+import React from "react"
+import "./App.css"
+
+
+function App() {
+   
+  return (
+    <div className="mytodoapp">
+      <Header/>
+      <Todo/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Form.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import styles from "./form.module.css";
+
+function Form({ todos, setTodos }) {
+  // const [todo, setTodo] = useState("");
+  const [todo, setTodo] = useState({name:"", done:false});
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setTodos([...todos, todo]);
+    setTodo({name:"", done:false});
+  }
+
+  return (
+    <form className={styles.todoform} onSubmit={handleSubmit}>
+      <div className={styles.inputContainer}>
+        <input
+          className={styles.modernInput}
+          value={todo.name}
+          type="text"
+          onChange={(e) => setTodo({name: e.target.value, done:false})}
+          placeholder="Enter todo item..."
+        />
+        <button className={styles.modernButton} type="submit">
+          Add
+        </button>
+      </div>
+    </form>
+  );
+}
+
+export default Form;
